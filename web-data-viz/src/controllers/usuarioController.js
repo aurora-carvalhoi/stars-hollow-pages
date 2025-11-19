b49faf3b15aa9d99b450c289b20022c48a5eb0ff
aurora-carvalhoi <aurora.inacio@sptech.school>
2025-11-19 19:13:58 -0300

    cadastro e login integrados com o banco de dados, .env commitado sem os dados do BD por segurança

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -1,17 +1,17 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+// var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
+    var nomeUsuario = req.body.nomeUsuarioServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
+    if (nomeUsuario == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
 
-        usuarioModel.autenticar(email, senha)
+        usuarioModel.autenticar(nomeUsuario, senha)
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
@@ -19,21 +19,22 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
+                        res.json(resultadoAutenticar)
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                        //     .then((resultadoAquarios) => {
+                        //         if (resultadoAquarios.length > 0) {
+                        //             res.json({
+                        //                 id: resultadoAutenticar[0].id,
+                        //                 email: resultadoAutenticar[0].email,
+                        //                 nome: resultadoAutenticar[0].nome,
+                        //                 senha: resultadoAutenticar[0].senha,
+                        //                 aquarios: resultadoAquarios
+                        //             });
+                        //         } else {
+                        //             res.status(204).json({ aquarios: [] });
+                        //         }
+                        //     })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -53,24 +54,40 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+       var primeiroNome = req.body.nomeServer
+       var email = req.body.emailServer
+       var nomeUsuario = req.body.nomeUsuarioServer
+       var dtNascimento = req.body.dtNascimentoServer
+       var fkPersonagemFav = req.body.personagemFavServer
+       var fkBoyFav = req.body.boyFavServer
+       var fkIvyLeague = req.body.ivyLeagueServer
+       var qtdViews = req.body.qtdViewsServer
+       var senha = req.body.senhaServer
+    // primeiroNome, email, nomeUsuario, dtNascimento, fkPersonagemFav,fkBoyFav, fkIvyLeague, qtdViews, senha
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    if (primeiroNome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else {
+    } else if (nomeUsuario == undefined) {
+        res.status(400).send("Seu nome de usuário está undefined!");
+    } else if (dtNascimento == undefined) {
+        res.status(400).send("Sua data de nascimento está undefined!");
+    } else if (fkPersonagemFav == undefined) {
+        res.status(400).send("Seu personagem favorito está undefined!");
+    }else if (fkBoyFav == undefined) {
+        res.status(400).send("Seu team está undefined!");
+    }else if (fkIvyLeague == undefined) {
+        res.status(400).send("Sua Ivy League está undefined!");
+    } else if (qtdViews == undefined) {
+        res.status(400).send("A quantidade de vezes que você viu a série está undefined!");
+    }else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(primeiroNome, email, nomeUsuario, dtNascimento, fkPersonagemFav,fkBoyFav, fkIvyLeague, qtdViews, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
