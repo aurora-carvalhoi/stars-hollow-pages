c8f6cc593684daff93db819a0c684c759af5e904
aurora-carvalhoi <aurora.inacio@sptech.school>
2025-12-07 18:12:05 -0300

    tinha esquecido de adicionar o script das tabelas do banco e só percebi agr, então último commit de última hora kkkk

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,62 +1,110 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+USE stars_hollow_pages;
 
-/*
-comandos para mysql server
-*/
+CREATE TABLE personagem (
+idPersonagem INT PRIMARY KEY,
+nome VARCHAR(20) NOT NULL,
+categoria VARCHAR(6) NOT NULL,
+caminhoPersonagem VARCHAR(255) NOT NULL,
+CONSTRAINT chkCategoria 
+CHECK (categoria in("avatar","boy")));
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+CREATE TABLE ivyLeague (
+idIvy INT PRIMARY KEY,
+nome VARCHAR(20) NOT NULL,
+caminhoIvy VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+primeiroNome VARCHAR(45) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+nomeUsuario VARCHAR(15) NOT NULL UNIQUE,
+dtNascimento DATE NOT NULL,
+fkPersonagemFav INT NOT NULL,
+fkBoyFav INT NOT NULL,
+fkIvyLeague INT NOT NULL,
+qtdViews INT NOT NULL,
+senha VARCHAR(45) NOT NULL,
+CONSTRAINT fkFavoritoUsuario
+FOREIGN KEY (fkPersonagemFav) REFERENCES personagem (idPersonagem),
+CONSTRAINT fkBoyUsuario
+FOREIGN KEY (fkBoyFav) REFERENCES personagem (idPersonagem),
+CONSTRAINT fkIvyUsuario
+FOREIGN KEY (fkIvyLeague) REFERENCES ivyLeague (idIvy)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE resultadoQuizz (
+idResposta INT,
+fkUsuario INT,
+pontuacaoConhecimento INT,
+dtQuizz DATE,
+CONSTRAINT pkComposta
+PRIMARY KEY (idResposta, fkUsuario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+DESCRIBE personagem;
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+INSERT INTO personagem VALUES 
+(1, "Lorelai", "avatar", "./assets/imagens/imgs_bd/lorelai.png"),
+(2, "Rory", "avatar", "./assets/imagens/imgs_bd/rory.png"),
+(3, "Luke", "avatar", "./assets/imagens/imgs_bd/luke.png"),
+(4, "Lane", "avatar", "./assets/imagens/imgs_bd/lane.png"),
+(5, "Paris", "avatar", "./assets/imagens/imgs_bd/paris.png"),
+(6, "Richard", "avatar", "./assets/imagens/imgs_bd/richard.png"),
+(7, "Emily", "avatar", "./assets/imagens/imgs_bd/emily.png"),
+(8, "Sookie", "avatar", "./assets/imagens/imgs_bd/sookie.png"),
+(9, "Kirk", "avatar", "./assets/imagens/imgs_bd/kirk.png"),
+(10, "Michel", "avatar", "./assets/imagens/imgs_bd/michel.png"),
+(11, "Dean", "boy", "./assets/imagens/imgs_bd/dean.png"),
+(12, "Jess", "boy", "./assets/imagens/imgs_bd/jess.png"),
+(13, "Logan", "boy", "./assets/imagens/imgs_bd/logan.png");
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+INSERT INTO ivyLeague VALUES 
+(1, "Harvard", "./assets/imagens/imgs_bd/harvard.png"),
+(2, "Yale", "./assets/imagens/imgs_bd/yale.png"),
+(3, "Princeton", "./assets/imagens/imgs_bd/princeton.png");
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+
+describe usuario;
+
+SELECT * FROM resultadoQuizz;
+
+ALTER TABLE resultadoQuizz MODIFY COLUMN dtQuizz DATE DEFAULT (current_date);
+
+ALTER TABLE resultadoQuizz MODIFY COLUMN idResposta INT AUTO_INCREMENT;
+
+ALTER TABLE resultadoQuizz MODIFY COLUMN pontuacaoConhecimento DECIMAL(5,2);
+
+select MAX(pontuacaoConhecimento) FROM resultadoQuizz
+where fkUsuario = 1;
+
+select round(avg(pontuacaoConhecimento),2) as "media_pontuacao_quizz" from resultadoQuizz;
+
+select count(idResposta) as "Quantidade de tentativas" from resultadoQuizz where fkUsuario = 1;
+
+select il.nome as nomeIvy, COUNT(*) as qtd_usuarios FROM usuario JOIN ivyLeague as il on usuario.fkIvyLeague = il.idIvy GROUP BY il.nome;
+
+select qtdViews, COUNT(*) as qtd_usuarios from usuario group by qtdViews;
+
+select caminhoPersonagem from personagem join usuario on fkPersonagemFav = idPersonagem WHERE idUsuario = 1;
+
+select caminhoIvy from ivyLeague join usuario on fkIvyLeague = idIvy WHERE idUsuario = 1;
+
+select caminhoPersonagem from personagem join usuario on fkBoyFav = idPersonagem WHERE idUsuario = 1;
+
+select nomeUsuario from usuario where idUsuario = 1;
+
+select dtQuizz, pontuacaoConhecimento from resultadoQuizz join usuario on fkUsuario = idUsuario where idUsuario = 1;
+
+select * from usuario;
+
+
+
+
+
+
+
+
+
+
+
